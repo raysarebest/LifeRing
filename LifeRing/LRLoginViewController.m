46f7d3c322aee840dad5cd5c5121f45001d6c880
connor <scott@seas.1259.apng.wireless-pennnet.upenn.edu>
2015-01-17 13:01:12 -0500

    full finished login and sign up

--- LifeRing/LRLoginViewController.m
+++ LifeRing/LRLoginViewController.m
@@ -13,13 +13,20 @@
 
 
 
--(IBAction)logIn{NSString *email = [self.emailField.text stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
-NSString *password = [self.passwordField.text stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
-
-//parse shit here//
-[PFUser logInWithUsernameInBackground:email password:password block:^(PFUser *user, NSError *error) {
+-(IBAction)logIn{
+    NSString *email = [self.emailField.text stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+    NSString *password = [self.passwordField.text stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+    //parse shit here//
+    [PFUser logInWithUsernameInBackground:email password:password block:^(PFUser *user, NSError *error) {
     
 }];
+    if ([email length] == 0 || [password length] == 0) {
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Login Error" message:@"You must enter your email address & password" preferredStyle:UIAlertControllerStyleAlert];
+        UIAlertAction *ok = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil];
+        [alert addAction:ok];
+        [self presentViewController:alert animated:YES completion:nil];
+    }
+
 
 }
 
--- LifeRing/LRSignUpViewController.m
+++ LifeRing/LRSignUpViewController.m
@@ -7,14 +7,31 @@
 //
 
 #import "LRSignUpViewController.h"
+@import Parse;
 @interface LRSignUpViewController()
 @property (strong, nonatomic) NSArray *problems;
 @end
 @implementation LRSignUpViewController
 -(IBAction)signUp{
-    
+    NSString *firstName = [self.firstNameField.text stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+    NSString *lastName = [self.lastNameField.text stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+    NSString *email = [self.emailField.text stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+    NSString *password = [self.passwordField.text stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+    NSString *confirmPassword = [self.confirmPasswordField.text stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+    if ([email length] == 0 || [password length] == 0) {
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Login Error" message:@"You must enter your email address & password" preferredStyle:UIAlertControllerStyleAlert];
+        UIAlertAction *ok = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil];
+        [alert addAction:ok];
+        [self presentViewController:alert animated:YES completion:nil];
+    }
+    else{
+        PFUser *newUser = [PFUser user];
+        newUser.email = email;
+        newUser.password = password;
+    }
 }
-#pragma mark - UITableViewDataSource Protocol Methods
+
+   #pragma mark - UITableViewDataSource Protocol Methods
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
     NSLog(@"%ld", (long)section);
     return 1;
